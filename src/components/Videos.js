872c4bdd44f3e1a4fcfe503f8770b6bee3c9cdb8
Shafiq <shafiq523000@gmail.com>
2022-11-05 15:50:51 +0600

    video list show and infinity scroll done

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,25 +1,33 @@
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
-import classes from "../styles/Videos.module.css";
+import useVideoList from "../hooks/useVideoList";
 import Video from "./Video";
 
 export default function Videos() {
+  const [page, setPage] = useState(1);
+  const { loading, videos, error, hasMore } = useVideoList(page);
+  console.log("page", videos);
+
   return (
-    <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-    </div>
+    <>
+      {videos.length > 0 && (
+        <InfiniteScroll dataLength={videos.length} next={() => setPage(page + 8)} hasMore={hasMore}>
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video video={video} />
+              </Link>
+            ) : (
+              <Video video={video} key={video.youtubeID} />
+            )
+          )}
+          ;
+        </InfiniteScroll>
+      )}
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was an error!</div>}
+      {loading && <div>Loading...</div>}
+    </>
   );
 }
---
+++ src/hooks/useVideoList.js
@@ -0,0 +1,38 @@
+import { get, getDatabase, limitToFirst, orderByKey, query, ref, startAt } from "firebase/database";
+import { useEffect, useState } from "react";
+
+export default function useVideoList(page) {
+  const [loading, setLoading] = useState(true);
+  const [videos, setVideos] = useState([]);
+  const [error, setError] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    async function fetchVideos() {
+      // database related works
+      const db = getDatabase();
+      const videosRef = ref(db, "videos");
+      const videoQuery = query(videosRef, orderByKey(), startAt("" + page), limitToFirst(8));
+      try {
+        setError(false);
+        //request firebase database
+        const snapshot = await get(videoQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setVideos((prevVideos) => {
+            return [...prevVideos, ...Object.values(snapshot.val())];
+          });
+        } else {
+          setHasMore(false);
+        }
+      } catch (error) {
+        console.log("error", error);
+        setLoading(false);
+        setError(true);
+      }
+    }
+    fetchVideos();
+  }, [page]);
+
+  return { loading, videos, error, hasMore };
+}
